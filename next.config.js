59038b683550160f0cd54400a1925a521b4cbac6
Ryan Rossiter <nayr0788@gmail.com>
2019-07-25 20:25:45 -0400

    Fix asset prefix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/fractalkompute-logo' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/shape-extrusion-zdog' : '',
 }
